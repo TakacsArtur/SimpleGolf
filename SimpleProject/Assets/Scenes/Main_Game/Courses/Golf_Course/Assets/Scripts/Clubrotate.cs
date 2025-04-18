7faf395b2f8bdb86230090c39b0b0fadb235a0d0
TakacsArtur <takacs.artur@outlook.com>
2025-04-18 12:56:23 +0200

    sorted physics a bit

--- SimpleProject/Assets/Scenes/Main_Game/Courses/Golf_Course/Assets/Scripts/Clubrotate.cs
+++ SimpleProject/Assets/Scenes/Main_Game/Courses/Golf_Course/Assets/Scripts/Clubrotate.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,17 @@ public class Clubrotate : MonoBehaviour
 {
     // Start is called before the first frame update
     public int rotationMultiplier = 1;
+    private Rigidbody rigidbody;
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Rotate(rotationMultiplier * Input.GetAxis("Vertical") * -1*Vector3.forward, Space.World);
+        rigidbody.maxAngularVelocity = float.MaxValue;
+        rigidbody.AddTorque(rotationMultiplier * Input.GetAxis("Vertical")* -1 *Vector3.forward);
+        
     }
 }
